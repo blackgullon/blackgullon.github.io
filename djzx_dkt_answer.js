0066568693fe470f75005eaee8f87f545e0d5572
blackgullon <49737340+blackgullon@users.noreply.github.com>
2019-09-28 00:34:11 +0800

    Add files via upload

---
+++ djzx_dkt_answer.js
@@ -0,0 +1,66 @@
+﻿var answerList = [];
+var params = {};
+var examId = "";
+var yongtime = Math.ceil(10000 + 4*Math.random()*1000) + 2000;
+
+$(document).ready(function() {
+	setTimeout('$(".answerConfirm").click()',2000);
+	init_data();
+	setTimeout(jiaojuan,yongtime);
+});
+
+function init_data(){
+	var allData = JSON.parse(sessionStorage.getItem('allData')).data;
+	examId = allData[0].examId;
+	for(var i=0;i<allData.length;i++){
+		var answer = {};
+		answer.id = allData[i].id;
+		answer.answer = "";
+		for(var j=0;j<allData[i].options.length;j++){
+			if(allData[i].options[j].checked == true) answer.answer += allData[i].options[j].optionName;
+		}
+		answerList.push(answer);
+	}
+  
+	params = {
+    "userId": userId,
+    "examId": examId,
+    'questionList':answerList,
+    'useTime': yongtime || ''
+	}
+}
+
+function jiaojuan(){
+	$.ajax({
+      url: "http://dkt.dtdjzx.gov.cn/examination/postExamination",
+      type: 'post',
+      data:JSON.stringify(params),
+      dataType: "json",
+      contentType: 'application/json',
+	  success: function(res){
+		dats=res;
+		if(dats.code != 200) {
+			alert(dats.message);
+			return;
+		}
+		sessionStorage.setItem('huikanData',JSON.stringify(dats));
+		var data = res.data;
+		var questionTotal = data.reminisceQuestionList.length;
+		var totalScore = data.totalScore;
+		var rightCount = data.rightCount;
+		var wrongCount = data.wrongCount;
+		clearInterval(jisiqi);
+		$('#mydefen').modal("show");
+		$('.fenfen').html(totalScore); //分数
+		// localStorage.setItem('fenfen',w_fff)
+		$('.w_shijain').html( $('.W_time').html() );
+		// localStorage.setItem('w_shijain', $('.W_time').html() )
+		$('.w_num_geshu').html( questionTotal );
+		$('.w_dui').html(rightCount);
+		$('.w_cuode').html(wrongCount);
+
+		$('.jiaojuan').addClass('W_jiaoquancol');
+	  }
+	});  
+	
+}
