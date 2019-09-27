7361c1862806e44089a4ca642882746b354a4226
blackgullon <49737340+blackgullon@users.noreply.github.com>
2019-09-28 01:49:51 +0800

    Update djzx_dkt.js

--- djzx_dkt.js
+++ djzx_dkt.js
@@ -265,7 +265,6 @@ function init_enable(){
 }
 function init_compontent() {
 	$(".header").append("<div id='messageContent' style='width:1050px;padding:10px 10px;background-color:#fff;margin: 0 auto;line-height:30px;height:200px;'></div>");
-	$("#messageContent").css("height","140px");
     var lblText = "请选择开始课程：";
 	var courseSelect = "<select id='courseSelect' style='width:500px;height:30px;' ></select>&nbsp;&nbsp;&nbsp;&nbsp;";
 	var publishMonth = "<select id='publishMonth' style='width:80px;height:30px;' ></select>&nbsp;&nbsp;&nbsp;&nbsp;";
@@ -274,6 +273,7 @@ function init_compontent() {
 	$("#messageContent").append("<div>" + lblText + courseSelect + publishMonth + btnStart + btnEnd + "</div>");
     $("#Start").bind("click",
 		function() {
+			$("#messageContent").css("height","140px");
 			init_disable();
 			if(validateSet() == false){
 				init_enable();
