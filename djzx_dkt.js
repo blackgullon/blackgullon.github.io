390792879538af699c348607063fa6d23db53391
blackgullon <49737340+blackgullon@users.noreply.github.com>
2019-09-29 21:54:06 +0800

    Update djzx_dkt.js

--- djzx_dkt.js
+++ djzx_dkt.js
@@ -266,7 +266,7 @@ function init_enable(){
 	$("#publishMonth").removeAttr("disabled");
 }
 function init_compontent() {
-	$(".header").append("<div id='messageContent' style='width:1050px;padding:10px 10px;background-color:#fff;margin: 0 auto;line-height:30px;height:200px;'></div>");
+	$(".header").append("<div id='messageContent' style='width:1200px;padding:10px 10px;background-color:#fff;margin: 0 auto;line-height:30px;height:200px;'></div>");
     var lblText = "请选择开始课程：";
 	var courseSelect = "<select id='courseSelect' style='width:500px;height:30px;' ></select>&nbsp;&nbsp;&nbsp;&nbsp;";
 	var publishMonth = "<select id='publishMonth' style='width:80px;height:30px;' ></select>&nbsp;&nbsp;&nbsp;&nbsp;";
