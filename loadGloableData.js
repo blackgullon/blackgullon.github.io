2278c3ba615ab3fd8d71df998c6e9bf837067057
blackgullon <49737340+blackgullon@users.noreply.github.com>
2019-09-30 02:34:11 +0800

    Add files via upload

---
+++ loadGloableData.js
@@ -0,0 +1,15 @@
+﻿function setData(){
+	if(sessionStorage.getItem("userInfo") == null) {
+		setTimeout(setData,1000);
+	}
+	var baseInfo = {};
+	baseInfo.userInfo = JSON.parse(sessionStorage.getItem("userInfo")).data;
+	baseInfo.playFlag = appToken.token + "undefined";
+	
+	var div=document.createElement("div");
+	div.id = "baseInfo";
+	div.style.display = "none";
+	div.appendChild(document.createTextNode(JSON.stringify(baseInfo)));  
+	document.body.appendChild(div);
+}
+setData();
