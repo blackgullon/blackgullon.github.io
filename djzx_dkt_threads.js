7c28a42d06a562b075d68f9c247f420d40b5ca01
blackgullon <49737340+blackgullon@users.noreply.github.com>
2020-01-31 11:51:06 +0800

    Update djzx_dkt_threads.js

--- djzx_dkt_threads.js
+++ djzx_dkt_threads.js
@@ -7,7 +7,7 @@ var speedTimes = 1;
 var updateendMaxCount = 5;
 var updateendFlagCount = 0;
 var taskProcessList = [];
-var asyncCount = 5;
+var asyncCount = 20;
 
 //新变量
 var baseInfo = {};
@@ -32,7 +32,7 @@ function setGloableData(){
 //finish
 function init_baseInfo() {
     baseInfo.pageSize = 5000;
-    baseInfo.publishMonth = "201912";
+    baseInfo.publishMonth = "202001";
     baseInfo.userInfo = JSON.parse($("#baseInfo").html()).userInfo;
     baseInfo.playFlag = JSON.parse($("#baseInfo").html()).playFlag;
     baseInfo.totalTime = -1;
