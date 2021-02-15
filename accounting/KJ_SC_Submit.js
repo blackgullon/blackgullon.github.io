b57e646255b55739e29d46e43f3d3150f6760f7a
blackgullon <49737340+blackgullon@users.noreply.github.com>
2021-02-15 13:12:34 +0800

    Update KJ_SC_Submit.js

--- accounting/KJ_SC_Submit.js
+++ accounting/KJ_SC_Submit.js
@@ -1,19 +1,19 @@
-function toPrintScore(){
-	var thisUrl = window.location.pathname.split('/');
-	window.location = window.location.origin + "/Learning/PrintScore/" + thisUrl[thisUrl.length-1];
-}
-var jsonAnswer = {};
-function autoSubmit(){
-	if($("#getAnswer").val() == undefined || $("#getUserId").val() == ""){
+//function toPrintScore(){
+//	var thisUrl = window.location.pathname.split('/');
+//	window.location = window.location.origin + "/Learning/PrintScore/" + thisUrl[thisUrl.length-1];
+//}
+//var jsonAnswer = {};
+//function autoSubmit(){
+//	if($("#getAnswer").val() == undefined || $("#getUserId").val() == ""){
 	
-		return;
-	}
-	else{
-		clearInterval(window.autoSubmitTime);
-		jsonAnswer = JSON.parse($("#getAnswer").val());
+//		return;
+//	}
+//	else{
+//		clearInterval(window.autoSubmitTime);
+//		jsonAnswer = JSON.parse($("#getAnswer").val());
 		//setTimeout(function(){ajaxPostExam(jsonAnswer);setTimeout("toPrintScore()",3000);},3000)
 		//5秒后转到打印成绩页面
-	}
-}
+//	}
+//}
 
 //window.autoSubmitTime = setInterval("autoSubmit()",1000);
